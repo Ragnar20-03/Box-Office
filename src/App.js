f4708e1d84ddb3a0b83bd5d4b22225d15276ff82
Roshan Prashant patil <roshanpp20@gmail.cpm>
2023-01-02 14:24:35 -0800

    change format

--- src/App.js
+++ src/App.js
@@ -4,26 +4,15 @@ import { Route } from "react-router-dom";
 import Nav from "./components/Nav";
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path={"/"}>
-          S This is home page
-        </Route>
-
-        <Route exact path="/aaaa">
-          This is AAAA home page
-        </Route>
-
-        <Route exact path={"/start"}>
-          This isStart Page
-        </Route>
-
-        <Route>THis is errrorororo</Route>
-      </Switch>
-      <Nav />
-    </div>
-  );
+  return <div>
+    <Switch>
+      <Route exact path={"/"}>This is home page</Route>
+      <Route exact path={"/1st"}>This is 1stw  page</Route>
+      <Route exact path={"/2nd"}>This is 2ndw page</Route>
+      <Route >This is 2ndw page</Route>
+    </Switch>
+    <Nav/>
+  </div>
 }
 
 export default App;
