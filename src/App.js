2f1d1b0efb18016df190fb7857ed2c70917bc015
Roshan Prashant patil <roshanpp20@gmail.cpm>
2023-01-06 23:04:15 -0800

    Created Show page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import { Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
+import Show from "./pages/Show";
 
 function App() {
   return (
+    <Switch>
+      <Route exact path={"/"}>
+        <Home />
+      </Route>
+      <Route exact path={"/Starred"}>
+        <Starred />
+      </Route>
 
-      <Switch>
-        <Route exact path={"/"}>
-          <Home />
-        </Route>
-        <Route exact path={"/Starred"}>
-          <Starred />
-        </Route>
-
-        <Route><div>Not Found 404</div></Route>
-      </Switch>
-
+      <Route exact path={"/show/:id"}>
+        <Show/>
+      </Route>
+      <Route>
+        <div>Not Found 404</div>
+      </Route>
+    </Switch>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import MainPageLayout from "../components/MainPageLayout";
 import { apiGet } from "../misc/config";
 import ShowGrid from "../components/show/ShowGrid";
@@ -12,6 +12,10 @@ const Home = () => {
   const [searchOption, setSearchOption] = useState("shows");
 
   const isShowsSearch = searchOption === "shows";
+
+  useEffect(()=>{
+    console.log('useEffectRun');
+  } , [])
 
   const onSearch = () => {
     apiGet(`/search/${searchOption}?q=${input}`).then((results) => {
