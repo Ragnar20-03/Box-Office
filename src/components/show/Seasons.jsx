94ebca33e8eed853f51242967b6a67370ff64d13
Roshan Prashant patil <roshanpp20@gmail.cpm>
2023-01-09 10:54:07 -0800

    Added Styling to Read More Show Cards

---
+++ src/components/show/Seasons.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { SeasonList, SeasonsWrapper } from './seasons.styled';
+
+
+const Seasons = ({ seasons }) => {
+  return (
+    <SeasonsWrapper>
+      <p>
+        Seasons in total: <span>{seasons.length}</span>
+      </p>
+      <p>
+        Episodes in total:{' '}
+        <span>
+          {seasons.reduce((acc, season) => acc + season.episodeOrder, 0)}
+        </span>
+      </p>
+      <SeasonList>
+        {seasons.map(season => (
+          <div className="season-item" key={season.id} >
+            <div className="right">
+              <p>Season {season.number}</p>
+              <p>
+                Episodes: <span>{season.episodeOrder}</span>
+              </p>
+            </div>
+            <div className="left">
+              Aired:{' '}
+              <span>
+                {season.premiereDate} - {season.endDate}
+              </span>
+            </div>
+          </div>
+        ))}
+      </SeasonList>
+    </SeasonsWrapper>
+  );
+};
+
+export default Seasons;
---
+++ src/components/show/ShowMainData.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import IMG_PLACEHOLDER from "../../images/not-found.png";
+import { Star } from "../style";
+import { MainDataWrapper, TagList } from "./showMainData.styled";
+
+const ShowMainData = ({ name, rating, summary, tags, image }) => {
+  return (
+    <MainDataWrapper>
+      <img src={image ? image.original : IMG_PLACEHOLDER} alt="show-cover" />
+      <div className="text-side">
+        <div className="Headline ">
+          <h1>{name}</h1>
+          <div>
+            <Star />
+            <span>{rating.average || "N/A"}</span>
+          </div>
+        </div>
+        <div
+          className="summary"
+          dangerouslySetInnerHTML={{ __html: summary }}
+        />
+
+        <div>
+          Tags:{" "}
+          <TagList>
+            {tags.map((tag, i) => (
+              <span key={i}>{tag}</span>
+            ))}
+          </TagList>
+        </div>
+      </div>
+    </MainDataWrapper>
+  );
+};
+export default ShowMainData;
